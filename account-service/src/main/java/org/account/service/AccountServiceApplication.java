8d0b7358179059b4a1011274e437360a9a8bf7d7
Mohamed Hafidi <mohamed.hafidi.mh@gmail.com>
2025-06-17 22:17:10 +0200

    communication between services

--- account-service/src/main/java/org/account/service/AccountServiceApplication.java
+++ account-service/src/main/java/org/account/service/AccountServiceApplication.java
@@ -3,46 +3,40 @@ package org.account.service;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.account.service.clients.CustomerRestClient;
 import org.account.service.entities.BankAccount;
 import org.account.service.enums.AccountType;
 import org.account.service.repository.BankAccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AccountServiceApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(AccountServiceApplication.class, args);
-    }
+	@Bean
+	CommandLineRunner commandLineRunner(BankAccountRepository accountRepository,
+			CustomerRestClient customerRestClient) {
+		return args -> {
+			customerRestClient.allCustomers().forEach(c -> {
+				BankAccount bankAccount1 = BankAccount.builder().accountId(UUID.randomUUID().toString()).currency("MAD")
+						.balance(Math.random() * 80000).createAt(LocalDate.now()).type(AccountType.CURRENT_ACCOUNT)
+						.customerId(c.getId()).build();
+				BankAccount bankAccount2 = BankAccount.builder().accountId(UUID.randomUUID().toString()).currency("MAD")
+						.balance(Math.random() * 65432).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT)
+						.customerId(c.getId()).build();
+				accountRepository.save(bankAccount1);
+				accountRepository.save(bankAccount2);
+			});
 
-    @Bean
-    CommandLineRunner commandLineRunner(BankAccountRepository accountRepository){
-        return args -> {
-                BankAccount bankAccount1 = BankAccount.builder()
-                        .accountId(UUID.randomUUID().toString())
-                        .currency("MAD")
-                        .balance(Math.random()*80000)
-                        .createAt(LocalDate.now())
-                        .type(AccountType.CURRENT_ACCOUNT)
-                        .customerId(Long.valueOf(1))
-                        .build();
-                BankAccount bankAccount2 = BankAccount.builder()
-                        .accountId(UUID.randomUUID().toString())
-                        .currency("MAD")
-                        .balance(Math.random()*65432)
-                        .createAt(LocalDate.now())
-                        .type(AccountType.SAVING_ACCOUNT)
-                        .customerId(Long.valueOf(2))
-                        .build();
-                accountRepository.save(bankAccount1);
-                accountRepository.save(bankAccount2);
-            ;
+		};
+	}
 
-
-        };
-    }
 }
--- account-service/src/main/java/org/account/service/controller/AccountRestController.java
+++ account-service/src/main/java/org/account/service/controller/AccountRestController.java
@@ -2,7 +2,9 @@ package org.account.service.controller;
 
 import java.util.List;
 
+import org.account.service.clients.CustomerRestClient;
 import org.account.service.entities.BankAccount;
+import org.account.service.model.Customer;
 import org.account.service.repository.BankAccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,21 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountRestController {
 	private BankAccountRepository accountRepository;
+	private CustomerRestClient customerRestClient;
 	
 	
 	
-	public AccountRestController(BankAccountRepository accountRepository) {
+	public AccountRestController(BankAccountRepository accountRepository, CustomerRestClient customerRestClient) {
 		this.accountRepository = accountRepository;
+		this.customerRestClient = customerRestClient;
 	}
 
 	@GetMapping("/accounts")
 	public List<BankAccount> accountList(){
-		return accountRepository.findAll();
+		List<BankAccount> accountList = accountRepository.findAll();
+		accountList.forEach(acc -> {
+			acc.setCustomer(customerRestClient.findCustomerById(acc.getCustomerId()));
+		});
+		return accountList;
 	}
 	
 	@GetMapping("/accounts/{id}")
 	public BankAccount bankAccountById(@PathVariable String id) {
-		return accountRepository.findById(id).get();
+		BankAccount bankAccount = accountRepository.findById(id).get();
+		Customer customer = customerRestClient.findCustomerById(bankAccount.getCustomerId());
+		bankAccount.setCustomer(customer);
+		return bankAccount;
 	}
 
 }
